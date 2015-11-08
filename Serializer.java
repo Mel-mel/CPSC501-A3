627f562488888bcf2204649b8023256b5cd42737
MelMel <la_la_la_la_1@hotmail.com>
2015-11-07 17:13:16 -0700

    Updated serializer
    Made it create proper syntax in xml file for field values and references

--- Serializer.java
+++ Serializer.java
@@ -22,43 +22,43 @@ public class Serializer {
 	private Document docs = null;
 	public Document serialize(Object obj)
 	{
-		Class classObject = obj.getClass();
+		Class<?> classObject = obj.getClass();
 		
 		Element serialized = new Element("serialized");
 		docs = new Document(serialized);
 		
 		String className = classObject.getName();
-		//Creating an unique id number for an object using it's className
-		//IdentityHashMap identity = new IdentityHashMap();
-		//identity.put(obj, className);
-		//object.setAttribute(new Attribute("id", Integer.toString(identity.hashCode())));
-		
+
 		Integer id = getObjID(obj);
 		
 		Element objectElem = new Element("object");
 		objectElem.setAttribute(new Attribute("class", className));
 		objectElem.setAttribute(new Attribute("id", id.toString()));
 		
-		docs.getRootElement().addContent(objectElem);
 		
 		if(classObject.isArray())
 		{
-			Object anArray = obj;
+			/*Object anArray = obj;
 			objectElem.setAttribute(new Attribute("length", Integer.toString(Array.getLength(anArray))));
 			if(classObject.getComponentType().isPrimitive())
 			{
-				Element value = new Element("value");
-				objectElem.addContent(new Element("value").setText(classObject.toString()));//Seems like it's null? from debug
+				System.out.println(classObject.getComponentType());
+				for (int i = 0; i < Array.getLength(anArray); i++)
+				{
+					System.out.println(Array.get(anArray, i).toString());
+					objectElem.addContent(new Element("value").setText(Array.get(anArray, i).toString()));
+				}
+				//objectElem.addContent(new Element("value").setText(classObject.toString()));//Seems like it's null? from debug
 			}
 			else
 			{
+				System.out.println(Array.getLength(anArray));
 				for(int i = 0; i < Array.getLength(anArray); i++)
 				{
 					id = getObjID(Array.get(anArray, i));
-					if(id != -1)
-					{
-						objectElem.addContent(new Element("reference").setText(id.toString()));
-					}
+					System.out.println("id " + id);
+					objectElem.addContent(new Element("reference").setText(id.toString()));
+					
 					//objectElem.addContent(new Element("reference").setText(Integer.toString(getObjID(Array.get(anArray, i)))));
 					
 					for(int k = 0; k < Array.getLength(anArray); k++)
@@ -66,12 +66,46 @@ public class Serializer {
 						serialize(Array.get(anArray, k));
 					}
 				}
-			}
+			}*/
 		}
 		else
 		{
-			Field[] listFields = classObject.getDeclaredFields();
+			Field[] list = classObject.getDeclaredFields();
+			for (int i = 0; i < list.length; i++)
+			{
+				serialField(list[i], obj, classObject, objectElem);
+			}
 			
+		}
+		
+		docs.getRootElement().addContent(objectElem);
+		return docs;
+	}
+	
+	private void serialField(Field field, Object obj, Class classObject, Element objectElem)
+	{
+		Element newField = new Element("field");
+		try {
+			if(!field.isAccessible())
+			{
+				field.setAccessible(true);
+			}
+			newField.setAttribute(new Attribute("name", field.getName()));
+			newField.setAttribute(new Attribute("declaringclass", obj.getClass().getName()));
+			if(field.getType().isPrimitive())
+			{
+				
+					String str = String.valueOf(field.get(obj));
+				
+				newField.addContent(new Element("value").setText(str));
+			}
+			else
+			{
+				Integer id = getObjID(field.get(obj));
+				newField.addContent(new Element("reference").setText(id.toString()));
+			}
+			/*
+			Field[] listFields = classObject.getDeclaredFields();
 			Element field = new Element("field");
 			
 			for(int i = 0; i < listFields.length; i++)
@@ -97,7 +131,7 @@ public class Serializer {
 				//and make it an object element. doesnt deal with arrays
 				else
 				{
-					Integer objID;
+				/*	Integer objID;
 					Field oneField = listFields[i];
 					try {
 						objID = getObjID(listFields[i].get(obj));
@@ -114,86 +148,16 @@ public class Serializer {
 					//serializeObject(obj, classObject);
 					
 				}
-			}
-			docs.getRootElement().addContent(field);
+			}*/
+		} 
+		catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		
-		
-		//serializeObject(obj, classObject, docs);
-		
-		
-		
-		return docs;
-	}
-	
-	private void serializeObject(Object obj, Class classObject, Document docs)
-	{
-		String className = classObject.getName();
-		//Creating an unique id number for an object using it's className
-		//IdentityHashMap identity = new IdentityHashMap();
-		//identity.put(obj, className);
-		//object.setAttribute(new Attribute("id", Integer.toString(identity.hashCode())));
-		
-		Integer id = getObjID(obj);
-		
-		Element objectElem = new Element("object");
-		objectElem.setAttribute(new Attribute("class", className));
-		objectElem.setAttribute(new Attribute("id", id.toString()));
-		
-		docs.getRootElement().addContent(objectElem);
-		if(classObject.isArray())
-		{
-			//make a method that deals with arrays
-		}
-		else
-		{
-			Class<?> c = classObject;
-			while(c != null)
-			{
-				serializeField(obj, classObject, docs);
-				c = c.getSuperclass();
-			}
-		}
-	}
-	
-	private void serializeField(Object obj, Class classObject, Document docs)
-	{
-		String className = classObject.getName();
-		Field[] listFields = classObject.getDeclaredFields();
-		
-		Element field = new Element("field");
-		
-		for(int i = 0; i < listFields.length; i++)
-		{
-			listFields[i].setAccessible(true);
-			
-			field.setAttribute(new Attribute("name", listFields[i].getName()));
-			field.setAttribute(new Attribute("declaringclass", listFields[i].getClass().getName()));
-			
-			if(listFields[i].getType().isPrimitive())
-			{
-				//Get value of a field and store its content as a value element
-				try {
-					field.addContent(new Element("value").setText(String.valueOf(listFields[i].get(obj))));
-				} catch (IllegalArgumentException e1) {
-					e1.printStackTrace();
-				} catch (IllegalAccessException e1) {
-					e1.printStackTrace();
-				}
-			}
-			
-			//When the field is not a primitive then it's an object so store its reference id
-			//and make it an object element.
-			else
-			{
-				field.addContent(new Element("reference").setText(Integer.toString(obj.hashCode())));
-				//serializeObject(obj, classObject);
-				
-			}
-		}
-		docs.getRootElement().addContent(field);
-
+		objectElem.addContent(newField);
 	}
 	
 	public void writeXMLFile(Document docs)
